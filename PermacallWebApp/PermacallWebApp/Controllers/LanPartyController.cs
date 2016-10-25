ead6d09140011db0c72e6e6b4834530dbc8e03d5
Youri Schuurmans <yourish@live.nl>
2016-10-25 13:02:57 +0200

    LanParty temporary shit

---
+++ PermacallWebApp/PermacallWebApp/Controllers/LanPartyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PermacallWebApp.Controllers
+{
+    public class LanPartyController : Controller
+    {
+        // GET: LanParty
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
